2f5083270198822bd303ee80cd87535316f786d9
Maksim Tropnikov <makstropnikov@gmail.com>
2022-04-14 17:32:46 +0700

    trying cookie

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -132,7 +132,10 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
         expiresIn: '7d',
       });
-      res.status(200).send({ token });
+      return res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7, httpOnly: true,
+      }).json({ message: 'Успешная авторизация' });
+      // res.status(200).send({ token });
       // res
       //   .cookie('jwt', token, {
       //     maxAge: 3600000 * 24,
